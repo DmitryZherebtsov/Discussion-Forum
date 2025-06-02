544c4105621b886209b1e29ab0d84936095eb369
Dmity <zherebtsovdima31@gmail.com>
2025-06-02 19:52:31 +0200

    style_changes

--- flaskstart/posts/routes.py
+++ flaskstart/posts/routes.py
@@ -70,7 +70,7 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been deleted!', 'success')
+    flash('Your post has been deleted!', 'danger')
     return redirect(url_for('main.home'))
     
 
